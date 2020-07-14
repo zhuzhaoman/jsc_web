bb3c13f7178b34552a4084133fd313f673b6ec21
zhuzhaoman <2358793988@qq.com>
2020-07-14 10:41:19 +0800

    1、增加域名搜索

--- src/views/device/domain-message/domain-search.vue
+++ src/views/device/domain-message/domain-search.vue
@@ -13,17 +13,33 @@
         style="width: 100%">
         <el-table-column
           prop="date"
-          label="日期"
+          label="槽位类型"
           width="180">
         </el-table-column>
         <el-table-column
           prop="name"
-          label="姓名"
+          label="板卡类型"
           width="180">
         </el-table-column>
         <el-table-column
           prop="address"
-          label="地址">
+          label="后IO卡类型">
+        </el-table-column>
+        <el-table-column
+          prop="address"
+          label="光接口卡类型">
+        </el-table-column>
+        <el-table-column
+          prop="address"
+          label="温度">
+        </el-table-column>
+        <el-table-column
+          prop="address"
+          label="板卡运行状态">
+        </el-table-column>
+        <el-table-column
+          prop="address"
+          label="采集时间">
         </el-table-column>
       </el-table>
     </div>
@@ -37,47 +53,13 @@
       return {
         domain: '',
         domain_id: '',
-        tableData: [{
-          date: '2016-05-02',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1518 弄'
-        }, {
-          date: '2016-05-04',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1517 弄'
-        }, {
-          date: '2016-05-01',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1519 弄'
-        }, {
-          date: '2016-05-03',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1516 弄'
-        }]
+        tableData: []
       }
     },
     mounted() {
 
     },
     methods: {
-      open() {
-        this.$prompt('请输入邮箱', '提示', {
-          confirmButtonText: '确定',
-          cancelButtonText: '取消',
-          inputPattern: /[\w!#$%&'*+/=?^_`{|}~-]+(?:\.[\w!#$%&'*+/=?^_`{|}~-]+)*@(?:[\w](?:[\w-]*[\w])?\.)+[\w](?:[\w-]*[\w])?/,
-          inputErrorMessage: '邮箱格式不正确'
-        }).then(({ value }) => {
-          this.$message({
-            type: 'success',
-            message: '你的邮箱是: ' + value
-          });
-        }).catch(() => {
-          this.$message({
-            type: 'info',
-            message: '取消输入'
-          });
-        });
-      }
     }
   }
 </script>
